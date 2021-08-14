7e41f2f4588211b3f4e1983ab8e6238624a7b3c3
sshar <shanharish96@gmail.com>
2021-08-14 18:04:21 -0400

    Dictionary

---
+++ Dictionary.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+def main():
+    animals = dict(kitten='meow', puppy='ruff!', lion='grrr',
+                   giraffe='I am a giraffe!', dragon='rawr')
+    for v in animals.values(): print(v)
+
+    # print_dict(animals)
+
+
+def print_dict(o):
+    for k, v in o.items(): print(f'{k}: {v}')
+
+    for x in o: print(f'{x}: {o[x]}')
+
+
+if __name__ == '__main__': main()
